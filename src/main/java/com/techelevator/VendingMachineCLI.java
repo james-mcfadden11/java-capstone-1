29da243374945aebd532dabb07d51f492385f91e
James McFadden <jamesmcfadden111@gmail.com>
2021-10-07 15:39:24 -0400

    More additions - changed switch's to if's

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -29,48 +29,38 @@ public class VendingMachineCLI {
 
 			} while (menuOneChoice < 1 || menuOneChoice > 3);
 
-			switch (menuOneChoice) {
-				case 1:
-					machine.printInventory();
-					break;
-				case 2:
-					// go to purchase menu
-					String menuTwoInput;
-					int menuTwoChoice = 0;
-					while (menuTwoChoice != 3) {
-						do {
-							System.out.println("1 Feed Money");
-							System.out.println("2 Select Product");
-							System.out.println("3 Finish Transaction");
-							System.out.println("Current Money Provided: $" + machine.getCurrentBalance());
-							menuTwoInput = keyboard.nextLine();
-							menuTwoChoice = Integer.parseInt(menuTwoInput);
-						} while (menuTwoChoice < 1 || menuTwoChoice > 3);
+			if (menuOneChoice == 1) {
+				machine.printInventory();
+			} else if (menuOneChoice == 2) {
+				// go to purchase menu
+				String menuTwoInput;
+				int menuTwoChoice = 0;
+				while (menuTwoChoice != 3) {
+					do {
+						System.out.println("1 Feed Money");
+						System.out.println("2 Select Product");
+						System.out.println("3 Finish Transaction");
+						System.out.println("Current Money Provided: $" + machine.getCurrentBalance());
+						menuTwoInput = keyboard.nextLine();
+						menuTwoChoice = Integer.parseInt(menuTwoInput);
+					} while (menuTwoChoice < 1 || menuTwoChoice > 3);
 
-						switch (menuTwoChoice) {
-							case 1:
-								System.out.println("Please enter the amount you would like to add: ");
-								String userAddedFunds = keyboard.nextLine();
-								int addedFunds = Integer.parseInt(userAddedFunds);
-								machine.addToBalance(addedFunds);
-								break;
-							case 2:
-								machine.printInventory();
-								System.out.println("Please enter the location of the product you would like to buy: ");
-								String itemLocation = keyboard.nextLine();
-								machine.sellItem(itemLocation);
-								break;
-							case 3:
-								machine.change();
-								break;
-						}
-
+					if (menuTwoChoice == 1) {
+						System.out.println("Please enter the amount you would like to add: ");
+						String userAddedFunds = keyboard.nextLine();
+						int addedFunds = Integer.parseInt(userAddedFunds);
+						machine.addToBalance(addedFunds);
+					} else if (menuTwoChoice == 2) {
+						machine.printInventory();
+						System.out.println("Please enter the location of the product you would like to buy: ");
+						String itemLocation = keyboard.nextLine();
+						machine.sellItem(itemLocation);
+					} else if (menuTwoChoice == 3) {
+						machine.change();
 					}
-					System.out.println("broke out of menu2 while loop");
-				case 3:
-					System.out.println("inside case 3 under menu1 while loop - exiting");
-					System.exit(0);
-					break;
+				}
+			} else if (menuOneChoice == 3) {
+				System.exit(0);
 			}
 		}
 	}
