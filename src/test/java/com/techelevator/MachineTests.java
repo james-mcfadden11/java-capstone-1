8b7f1784f65c47ce28d80daa485069878e077e28
timothy.ziegler <timothy.ziegler90@gmail.com>
2021-10-08 12:13:07 -0400

    started unit testing

---
+++ src/test/java/com/techelevator/MachineTests.java
@@ -0,0 +1,29 @@
+package com.techelevator;
+
+import org.junit.Test;
+
+public class MachineTests {
+    public static void main(String[] args) {
+
+    }
+
+    Item testItem = new Item("pop", 0.75, "B5", "Drink");
+
+@Test
+    public void balance_with_negative_number() {
+    // Arrange
+
+    // Act
+
+    
+}
+
+
+
+
+
+
+
+
+
+}
